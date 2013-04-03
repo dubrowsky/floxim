8e4a6053e0956c1532c1148157704275374eb5a6
epodluzhnyi <evgenyi@podluzhnyi.ru>
2013-04-03 13:08:03 +0400

    Удалил старый код в floxim/controller/infoblock.php

--- floxim/controller.php
+++ floxim/controller.php
@@ -30,7 +30,7 @@ class fx_controller {
 
     public function set_input($input) {
         $this->input = $input;
-        return $this;gbxf
+        return $this;
     }
     
     public function default_action() {
