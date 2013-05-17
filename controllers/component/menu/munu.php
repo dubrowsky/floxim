0b1ccef4fcfd76ebf16da75e9adbfe3d73474fc1
evgenyi <evgenyi@podluzhnyi.ru>
2013-05-17 19:09:32 +0400

    компонент меню

---
+++ controllers/component/menu/munu.php
@@ -0,0 +1,10 @@
+<?php
+class fx_controller_component_menu extends fx_controller_component {
+    public function do_listing() {
+        $this->listen('query_ready', function (fx_data $query) {
+            $query->with('tags');
+        });
+        return parent::do_listing();
+    }
+}
+?>
