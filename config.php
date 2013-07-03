c6867bca3f14c85dc146be9ee4c2d3f16018597f
Ilya Myasin <dubr.cola@gmail.com>
2013-07-03 18:26:50 +0400

    update&ignore config.php

--- config.php
+++ config.php
@@ -3,9 +3,9 @@
 define("FX_JQUERY_PATH", '/floxim/lib/js/jquery-1.9.1.js');
 $db_config = array(
     'default' =>  array(
-        'DB_DSN' => 'mysql:dbname=floxim_dev;host=localhost',
-        'DB_USER' => 'webmaster',
-        'DB_PASSWORD' => 'rikoJetti82'
+        'DB_DSN' => 'mysql:dbname=floxim;host=localhost',
+        'DB_USER' => 'root',
+        'DB_PASSWORD' => ''
     )
 );
 
