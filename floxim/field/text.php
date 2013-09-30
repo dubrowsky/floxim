a7f4b01a9965ec87e004f356a60585f3f0c4e0d7
dubrowsky <dubr.cola@gmail.com>
2013-09-30 18:16:02 +0400

    textarea with no wysiwyg for panel

--- floxim/field/text.php
+++ floxim/field/text.php
@@ -8,8 +8,8 @@ class fx_field_text extends fx_field_baze {
 
     public function get_js_field($content) {
         parent::get_js_field($content);
-
-        if (isset($this['format']) && isset($this['format']['html'])) {
+        dev_log('txt field', $this);
+        if (isset($this['format']) && isset($this['format']['html']) && $this['format']['html']) {
             $this->_js_field['wysiwyg'] = true;
         }
 
